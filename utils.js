d983f024ae31d086923bc4137af289c5e5e778bf
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2020-10-07 13:04:11 +0200

    Add files via upload

--- utils.js
+++ utils.js
@@ -12,3 +12,9 @@ function change_menu(
     document.getElementById("nav_" + menu_to_show).classList.add("selected");
     document.getElementById("page_" + menu_to_show).style.display = 'block';
 }
+
+if (Math.random() < 0.5) {
+    document.getElementsByTagName('head')[0].innerHTML += '<link rel="icon" type="image/png" href="lgo.png">');
+} else {
+    document.getElementsByTagName('head')[0].innerHTML += '<link rel="icon" type="image/png" href="lgom.png">');    
+}
