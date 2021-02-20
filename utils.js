7084c7f5d93de03cbed8c3fa5ea07cb03b7cd9e8
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2021-02-20 10:10:06 +0100

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,17 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    change_menu("home");
-});
-
-function change_menu(
-    menu_to_show
-) {
-    ['home', 'cv', 'pubs', 'contact'].forEach(function(memuee) {
-        document.getElementById("nav_" + memuee).classList.remove("selected");
-        document.getElementById("page_" + memuee).style.display = 'none';
-    });
-    document.getElementById("nav_" + menu_to_show).classList.add("selected");
-    document.getElementById("page_" + menu_to_show).style.display = 'block';
-}
 
 if (Math.random() < 0.5) {
     document.getElementsByTagName('head')[0].innerHTML += '<link rel="icon" type="image/png" href="lgo.png">';
